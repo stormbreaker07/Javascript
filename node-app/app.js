a6d77cea5c9385beb9295b68f0a9b53b52a6f764
stormbreaker07 <stormbreaker07>
2020-02-06 19:46:43 +0530

    npm validator module added

---
+++ node-app/app.js
@@ -0,0 +1,6 @@
+const validator = require('validator')
+const work = require('./util.js')
+
+const name = 'nodes project'
+console.log(validator.isEmail('tanuj07yadav07@gmail.com'))
+console.log(validator.isURL('abes.ac.in'))
---
+++ node-app/util.js
@@ -0,0 +1,7 @@
+
+var work = function(a,b)
+{return 'i don\'t make notes';}
+var name = 'tanuj yadav'
+console.log(name)
+
+module.exports = work;
