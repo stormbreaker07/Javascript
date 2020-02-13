dcb08b85a630e1d23f56dfa93fb25f4d0e0a3f9a
stormbreaker07 <stormbreaker07>
2020-02-14 02:47:24 +0530

    validator

--- node-app/app.js
+++ node-app/app.js
@@ -1,6 +1,8 @@
+const chalk = require('chalk')
 const validator = require('validator')
 const work = require('./util.js')
 
 const name = 'nodes project'
 console.log(validator.isEmail('tanuj07yadav07@gmail.com'))
-console.log(validator.isURL('abes.ac.in'))
+console.log(validator.isURL('abes'))
+console.log(chalk.redBright('hello world'))
